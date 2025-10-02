b922ac09e2390aeeea0a7326e39b4ae855e8facb
Aidana2007 <maksutova2007@inbox.ru>
2025-10-02 15:38:55 +0500

    implement deterministic select

---
+++ src/utils/ArrayUtils.java
@@ -0,0 +1,54 @@
+package utils;
+
+import java.util.Random;
+
+public class ArrayUtils {
+    private static Random random = new Random();
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high, int pivotIndex) {
+        int pivotValue = arr[pivotIndex];
+        swap(arr, pivotIndex, high);
+
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            Metrics.countComparisons();
+            if (arr[j] < pivotValue) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= left) {
+                Metrics.countComparisons();
+                if (arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
